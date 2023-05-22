a120e9417db3be8db8b06b26c56f478d7f5008d6
MattiaLR95 <laroccamattia@virgilio.it>
2023-05-22 14:56:38 +0200

    Risistemata grafica per la visualizzazione orizzontale dell'app

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -66,6 +66,8 @@ class _Expenses extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -85,12 +87,19 @@ class _Expenses extends State<Expenses> {
               onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _expenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _expenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _expenses)),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
