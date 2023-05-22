34815f6db6e028361504b5a39afbff975ca8bb08
MattiaLR95 <laroccamattia@virgilio.it>
2023-05-22 10:57:04 +0200

    Inseriti grafici nella homepage

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -86,7 +87,7 @@ class _Expenses extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(expenses: _expenses),
           Expanded(child: mainContent),
         ],
       ),
